dde7875d9c4b4aaafe55f86254237c6dc25dfff3
PriteshZade <155930242+PriteshZade@users.noreply.github.com>
2024-04-02 22:37:21 +0530

    Add files via upload

---
+++ Pairingelements.java
@@ -0,0 +1,16 @@
+public class Pairingelements {
+    public static void Pairing(int numbers[]) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                System.out.print("(" + numbers[i] + "," + numbers[j] + ")");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String args[]) {
+        int numbers[] = { 1, 2, 3, 4, 5, 6 };
+        Pairing(numbers);
+    }
+}
