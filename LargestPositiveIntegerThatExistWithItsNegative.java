7a51a0e74f4bf5603dc6b394cc59e0b9831a87c5
PriteshZade <155930242+PriteshZade@users.noreply.github.com>
2024-05-02 09:18:06 +0530

    Add files via upload

---
+++ LargestPositiveIntegerThatExistWithItsNegative.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findMaxK(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < j) {
+            if (-(nums[i]) == nums[j]) {
+                return nums[j];
+
+            }
+            if (-(nums[i]) < nums[j]) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return -1;
+    }
+}
